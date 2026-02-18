004e3414f6e149598bc1935be16b5f65b002f99a
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-02-18 18:13:28 +0530

    Assigned the frontend

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'binary-baniyas-secret-key'
 # Routes
 @app.route('/')
 def home():
-    return "<h1>DeceptiScan is Running! 🚀</h1><p>Member 2, replace this with index.html</p>"
+    return "<h1>DeceptiScan is Running! 🚀</h1><p>Pranav, replace this with index.html</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
