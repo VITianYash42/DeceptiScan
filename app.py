de80f745403765c56d3f5a72c15ec847a51ece72
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-02-18 18:07:03 +0530

    chore: Setup skeleton files and runnable app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'binary-baniyas-secret-key'
+
+# Routes
+@app.route('/')
+def home():
+    return "<h1>DeceptiScan is Running! 🚀</h1><p>Member 2, replace this with index.html</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
