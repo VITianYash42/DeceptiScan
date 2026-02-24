8cb6975dd16ced7a76b9a83316c3a91a3bd1de03
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-02-24 13:50:11 +0530

    Removed the js alerts

--- static/game.js
+++ static/game.js
@@ -1,3 +1,54 @@
+// pop up
+function showToast(message){
+
+    const container = document.getElementById("toast-container");
+    if(!container) return;
+
+    const toast = document.createElement("div");
+
+    // AUTO COLOR DETECTION
+    let color = "bg-blue-600";   // default
+
+    if(message.includes("+")) color="bg-green-600 ring-2 ring-green-300";
+    if(message.includes("-")) color="bg-red-600 ring-2 ring-red-300";
+
+    toast.className =
+        `${color} text-white px-6 py-4 rounded-xl shadow-2xl flex items-center gap-4 min-w-[280px] animate-toast-in`;
+
+    // message
+    const text = document.createElement("span");
+    text.innerText = message;
+    text.className = "flex-1";
+
+    // close button
+    const close = document.createElement("button");
+
+    close.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg"
+        class="w-4 h-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"/>
+    </svg>
+    `;
+    close.className =
+    "ml-2 opacity-70 hover:opacity-100 transition cursor-pointer";
+    close.onclick = ()=> toast.remove();
+
+    toast.appendChild(text);
+    toast.appendChild(close);
+
+    container.appendChild(toast);
+
+    // auto remove
+    setTimeout(()=>{
+        toast.classList.add("animate-toast-out");
+        setTimeout(()=>toast.remove(),300);
+    },5000);
+}
 let scenarios = [];
 let currentScenarioIndex = 0;
 let score = 0;
@@ -45,9 +96,9 @@ function clickLink(scenario){
 
     if(scenario.is_phish){
         score -= 50;
-        alert("❌ You clicked phishing link (-50)");
+        showToast("❌ You clicked phishing link (-50)");
     } else {
-        alert("✔ Safe link");
+        showToast("✔ Safe link");
     }
 
     sendResult(scenario.id, success);
@@ -63,10 +114,10 @@ function reportEmail(){
 
     if(scenario.is_phish){
         score += 100;
-        alert("✔ Correct phishing report (+100)");
+        showToast("✔ Correct phishing report (+100)");
     }else{
         score -= 20;
-        alert("❌ That email was safe (-20)");
+        showToast("❌ That email was safe (-20)");
     }
 
     sendResult(scenario.id, success);
@@ -88,7 +139,7 @@ async function sendResult(id, success){
         })
     });
 
-    alert(scenarios[currentScenarioIndex].explanation);
+    showToast(scenarios[currentScenarioIndex].explanation);
 }
 
 
@@ -98,7 +149,7 @@ function nextScenario(){
     currentScenarioIndex++;
 
     if(currentScenarioIndex >= scenarios.length){
-        alert("Game finished!");
+        showToast("Game finished!");
         return;
     }
 
